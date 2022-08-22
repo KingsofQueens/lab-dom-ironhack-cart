68804df7406a84fcafd28325de0271d2bc34581c
Q <morningqwhy@yahoo.com.hk>
2022-08-23 00:54:33 +0100

    WIP - Stuck at Iteration 3

--- js/index.js
+++ js/index.js
@@ -1,23 +1,68 @@
+// ITERATION 0
+
+/*window.addEventListener('load', () => {
+  const calculatePricesBtn = document.getElementById('calculate');
+  calculatePricesBtn.addEventListener('click', calculateAll);
+});*/
+
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  //Step 1
+  const priceElement = product.querySelector('.price span');
+  const quantityInputElement = product.querySelector('.quantity input');
 
-  //... your code goes here
+  //Step 2
+  price = priceElement.innerText;
+  quantity = quantityInputElement.value;
+
+  //Step 3
+  const subtotalCal = price * quantity;
+
+  //Step 4
+  const subtotal = product.querySelector('.subtotal span');
+
+  //Step 5
+  subtotal.innerText = subtotalCal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
+  /* const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
+  console.dir(singleProduct);
+  console.log(singleProduct);*/
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const productElements = document.getElementsByClassName('product');
+  for (const productElement of productElements) {
+    updateSubtotal(productElement);
+  }
 
   // ITERATION 3
   //... your code goes here
+  const calculatePricesBtn = document.getElementById('calculate');
+  const totalAmountElement = document.querySelector('#total-value span');
+  const subtotalElements = document.querySelectorAll('.subtotal span');
+  //console.log(subtotalElements);
+  //console.dir(subtotalElements);
+
+  let total = 0;
+
+  calculatePricesBtn.addEventListener('click', () => {
+    for (const subtotalElement of subtotalElements) {
+      let subtotal = Number(subtotalElement.innerText);
+      total += subtotal;
+    }
+  });
+
+  totalAmount = totalAmountElement.innerText;
+  //console.log(totalAmountElement);
+  //console.dir(totalAmountElement);
+
+  totalAmount = total;
 }
 
 // ITERATION 4
